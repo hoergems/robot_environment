e7b362fdd65d347ebe1568ccc8bc4bec88c957c6
hoergems <hoergems@gmail.com>
2016-06-27 21:05:15 +1000

    what

--- src/include/robot_environment.hpp
+++ src/include/robot_environment.hpp
@@ -88,9 +88,11 @@ public:
 
     double getSimulationStepSize() const;
 
-    template <class RobotType> std::shared_ptr<RobotEnvironment> clone() {
-        std::shared_ptr<RobotEnvironment> env = std::make_shared<RobotEnvironment>();
+    template <class RobotType> std::shared_ptr<shared::RobotEnvironment> clone() {
+	std::shared_ptr<shared::RobotEnvironment> env = std::make_shared<shared::RobotEnvironment>();
+        //RobotEnvironment* env(new RobotEnvironment());
         env->createRobot<RobotType>(robot_path_);
+	//env->setRobot(robot_);
         //env->loadEnvironment(environment_path_);
         env->setControlDuration(control_duration_);
         env->setSimulationStepSize(simulation_step_size_);
